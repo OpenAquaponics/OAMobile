beaf91253e7fac780115ca32ccf32b837be35716
jeliser <nestinator@gmail.com>
2013-03-24 20:34:50 -0500

    OANode sensor listing is nice and clean now.

--- AndroidJSONParsing/src/com/androidhive/jsonparsing/OAItem/OANodeItem.java
+++ AndroidJSONParsing/src/com/androidhive/jsonparsing/OAItem/OANodeItem.java
@@ -3,6 +3,7 @@ package com.androidhive.jsonparsing.OAItem;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ public class OANodeItem implements OAItem, OAMobileTags {
 
 	@SuppressWarnings("unchecked")
 	public OANodeItem(HashMap<String, String> map) {
+		// TODO: This should probably build up a logical object that has properties instead of using a String map. 
 		this.map = (HashMap<String, String>) map.clone();
 	}
 	
@@ -29,9 +31,31 @@ public class OANodeItem implements OAItem, OAMobileTags {
 			LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			View v = vi.inflate(R.layout.oanode_list, null);
 
-			((TextView)v.findViewById(R.id.sNodeId)).setText((String) map.get(NODE_ID));
-			((TextView)v.findViewById(R.id.sChannelNames)).setText((String) map.get(CHANNEL_NAMES));
-			((TextView)v.findViewById(R.id.sDescription)).setText((String) map.get(DESCRIPTION));
+			if(v.findViewById(R.id.sNodeId) != null)
+				((TextView)v.findViewById(R.id.sNodeId)).setText((String) map.get(NODE_ID));
+			if(v.findViewById(R.id.sChannelNames) != null)
+				((TextView)v.findViewById(R.id.sChannelNames)).setText((String) map.get(CHANNEL_NAMES));
+			if(v.findViewById(R.id.sDescription) != null)
+				((TextView)v.findViewById(R.id.sDescription)).setText((String) map.get(DESCRIPTION));
+			
+	        // Toggle the option colors
+			if(v.findViewById(R.id.sEnable) != null) {
+		        if(((String) map.get(ENABLED)).compareTo("1") == 0) {
+		        	((TextView) v.findViewById(R.id.sEnable)).setBackgroundColor(context.getResources().getColor(R.color.LightGreen));
+		        }
+		        else {
+		        	((TextView) v.findViewById(R.id.sEnable)).setBackgroundColor(context.getResources().getColor(R.color.LightRed));
+		        }
+			}
+	        
+			if(v.findViewById(R.id.sPublic) != null) {
+		        if(((String) map.get(PUBLIC)).compareTo("1") == 0) {
+		        	((TextView) v.findViewById(R.id.sPublic)).setText("Public");
+		        }
+		        else {
+		        	((TextView) v.findViewById(R.id.sPublic)).setText("Private");
+		        }
+			}
 			
 			return v;
 		}
